0fd26e32006e2549a37b96a2cd5d5aefa8a821eb
shen <santi.dionis@gmail.com>
2023-09-17 14:05:41 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB2"));
+//builder.Services.AddDbContext<TareasContext>(options => options.UseSqlServer("Data Source=(local); Initial Catalog= master;Trusted_Connection=True; Integrated Security=True"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+});
+
+app.Run();
