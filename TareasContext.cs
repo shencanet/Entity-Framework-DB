9b623cd92da6da5dde70bc49549bd5768e3840a4
shen <santi.dionis@gmail.com>
2023-09-26 20:57:09 +0200

    ADD: requerimientos prueba tecnica

--- TareasContext.cs
+++ TareasContext.cs
@@ -10,12 +10,12 @@ public class TareasContext: DbContext
 
     public TareasContext(DbContextOptions<TareasContext> options) :base(options) { }
     
-    public override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Categoria>().HasData(
+        modelBuilder.Entity<Categoria>(categoria=>{
             new Categoria { Id = 1, Nombre = "Trabajo" },
             new Categoria { Id = 2, Nombre = "Estudios" },
             new Categoria { Id = 3, Nombre = "Personal" }
-        );
+       });
     }
 }
