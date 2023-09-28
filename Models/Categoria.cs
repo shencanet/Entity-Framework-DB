02edd283203c6090f805c044f3fa60f5a20d41f5
shen <santi.dionis@gmail.com>
2023-09-28 20:32:54 +0200

    ADD: dotnet ef

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -12,5 +12,8 @@ public class Categoria
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
 
+    public int Peso {get;set;}
+
+
     public virtual ICollection<Tarea> Tareas {get;set;}
 }
